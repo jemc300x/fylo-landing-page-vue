9c828c4f3001e17f3db9d21636e10428ee08f6b4
jemc300x <jemc300x@gmail.com>
2020-05-16 20:14:22 -0500

    updated routes

--- src/router.js
+++ src/router.js
@@ -11,7 +11,9 @@ Vue.use(VueRouter)
 export default new VueRouter({
   routes:[
     {path: '/', component: Home},
-    {path: '/hola', component: TestPage},
+    {path: '/features', component: TestPage},
+    {path: '/singin', component: TestPage},
+    {path: '/about', component: TestPage},
     {path: '*', component: NotFound},
   ],
   mode: 'history'
